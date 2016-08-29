a7810157966ec2b35572fab87dfc26ee55a1019d
Juergen Bocklage-Ryannel <juergen.bocklage-ryannel@pelagicore.com>
2016-08-29 18:45:21 +0200

    updated demo cvs generator

--- generator/csv/csv.py
+++ generator/csv/csv.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
+import click
 from qface.generator import FileSystem, Generator
 
-system = FileSystem.parse_dir('./examples')
+
+def generate(input, output):
+    system = FileSystem.parse_dir(input)
+    generator = Generator(searchpath='./templates')
+    ctx = {'output': output, 'system': system}
+    generator.write('{{output}}/packages.csv', 'packages.csv', ctx)
 
 
-generator = Generator()
+@click.command()
+@click.option('--input', type=click.Path(exists=True))
+@click.option('--output', type=click.Path(exists=True))
+def runner(input, output):
+    generate(input, output)
 
-ctx = {'system': system}
-generator.write('out/packages.csv', 'packages.csv', ctx)
+if __name__ == '__main__':
+    runner()

0c0613f8b5719e396d6a053201c32482f95bdb7f
Juergen Bocklage-Ryannel <juergen.bocklage-ryannel@pelagicore.com>
2017-03-30 11:43:42 +0200

    simplified the dict merging. small fix

--- qface/utils.py
+++ qface/utils.py
@@ -1,15 +1,10 @@
 
 
-def merge(a, b, path=None):
-    "merges b into a"
-    # import pdb; pdb.set_trace()
-    path = path or []
+def merge(a, b):
+    "merges b into a recursively if a and b are dicts"
     for key in b:
-        if key in a:
-            if isinstance(a[key], dict) and isinstance(b[key], dict):
-                merge(a[key], b[key], path + [str(key)])
-            else:
-                a[key] = b[key]
+        if isinstance(a.get(key), dict) and isinstance(b.get(key), dict):
+            merge(a[key], b[key])
         else:
             a[key] = b[key]
     return a

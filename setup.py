cff641a9ff76970314db7835a2333e442437fe56
Juergen Bocklage-Ryannel <juergen@ryannel.org>
2018-03-07 19:11:09 +0100

    bumped antlr version to 4.7.1

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'click',
-        'antlr4-python3-runtime==4.7',
+        'antlr4-python3-runtime==4.7.1',
         'jinja2',
         'path.py',
         'pyyaml',

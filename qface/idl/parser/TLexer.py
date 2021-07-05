b0bcadea2c5c914fce872c8abaa78df1bfb0448c
Dominik Holland <dominik.holland@qt.io>
2021-07-05 19:20:30 +0200

    Disable the antlr runtim version check
    The current Lexer and Parser code also works with the latest version of
    antlr. We rely on the CI to detect any future problems.

--- qface/idl/parser/TLexer.py
+++ qface/idl/parser/TLexer.py
@@ -230,7 +230,9 @@ class TLexer(Lexer):
 
     def __init__(self, input=None, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.7.1")
+        # Disable the version check for now as although there is a mismatch the Lexer seems to work fine.
+        # Rely on the weekly CI to make sure this keeps working also with later antlr versions.
+        # self.checkVersion("4.7.1")
         self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
         self._actions = None
         self._predicates = None

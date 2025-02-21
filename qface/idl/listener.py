35455986e8ec2735b0a5f874dfdf2d8863d8007d
Shveta Mittal <shveta.mittal@qt.io>
2025-02-21 13:22:31 +0200

    QTBUG-129592: Throw error in case of invalid yaml format in annotations

--- qface/idl/listener.py
+++ qface/idl/listener.py
@@ -130,6 +130,7 @@ class DomainListener(QFaceListener):
         if ctx.tagSymbol():
             lines = [tag.line.text[1:] for tag in ctx.tagSymbol()]
             try:
+                self.validate_yaml(lines)
                 data = yaml.load('\n'.join(lines), Loader=Loader)
                 self.validate_keys(data)
                 symbol._tags = data
